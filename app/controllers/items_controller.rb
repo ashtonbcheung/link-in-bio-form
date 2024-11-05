bee4ef67cb96a6a10ba37a68d5bd853910ba534c
ashtonbcheung <70730294+ashtonbcheung@users.noreply.github.com>
2024-11-05 23:56:54 +0000

    finished

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,4 +4,23 @@ class ItemsController < ApplicationController
 
     render({ :template => "item_templates/list" })
   end
+
+  def display_form
+    render({ :template => "item_templates/display_form" })
+  end
+
+  def create_new_item
+    # Get the user's inputs out of params
+    # Create and save a new row in the items table
+    # Send the user onwards, to "/"
+    i = Item.new
+i
+    i.link_url = params.fetch("query_link_url")
+    i.link_description = params.fetch("query_link_description")
+    i.thumbnail_url = params.fetch("query_thumbnail_url")
+i
+    i.save
+
+    redirect_to("/")
+  end
 end
